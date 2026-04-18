42d41385aa811d8aec71ea49a4b837320d7df543
secum3r <42967816+secum3r@users.noreply.github.com>
2026-04-18 22:59:53 +1000

    Initial Commit

---
+++ PocketLuCI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketLuCI
+//
+//  Created by uq on 18/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketLuCI/PocketLuCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketLuCIApp.swift
+//  PocketLuCI
+//
+//  Created by uq on 18/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct PocketLuCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
